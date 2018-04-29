06b1ea8149d56aa064c3fdaad723728e8277d175
antoniotex <acm.skt@gmail.com>
2018-04-29 01:09:26 -0300

    Corrigido bug do undefined e refeito lógica

--- src/components/BuscaCep.vue
+++ src/components/BuscaCep.vue
@@ -1,20 +1,23 @@
 <template>
   <div>
-    {{cep}}
     <input class="teste" type="text" placeholder="Digite o CEP" v-model="cep" maxlength="8">
-    <p>{{posts.bairro}}</p>
+    <p>{{rua}}</p>
+    <p>{{cidade + uf}}</p>
     <p>{{erros}}</p>
   </div>
 </template>
 
 <script>
+
 import axios from 'axios'
 
 export default {
   name: 'BuscaCep',
   data () {
     return {
+      rua: '',
       cidade: '',
+      uf: '',
       cep: '',
       posts: [],
       erros: ''
@@ -24,30 +27,36 @@ export default {
     getCity: function () {
       axios.get('https://viacep.com.br/ws/' + this.cep + '/json').then(response => {
         this.posts = response.data
-        this.posts.erro === true ? this.erros = 'CEP NÃO ENCONTRADO' : console.log('falso')
+        if (this.posts.erro === true) {
+          this.erros = 'CEP NÃO ENCONTRADO'
+        } else {
+          this.rua = response.data.logradouro
+          this.cidade = response.data.localidade + ', '
+          this.uf = response.data.uf
+        }
       })
         .catch(e => {
-          console.log('teste erros')
+          this.erros.push(e)
         })
     }
   },
   watch: {
     cep: function () {
       if (this.cep.length === 8) {
-        console.log('Chegou em 8!!!')
         this.getCity()
         this.erros = ''
       }
-      if (this.cep.length < 8) {
-        console.log('menor que 8')
+      if (this.cep.length < 8 && this.cep.length > 0) {
+        this.erros = 'Cep invalido'
+        this.rua = ''
+        this.cidade = ''
+        this.uf = ''
+      } else {
+        this.erros = ''
       }
     }
-  },
-  mounted: function () {
-    this.getCity()
   }
 }
-console.log('Aqui chega 1')
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped></style>
